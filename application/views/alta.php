42baf1ecfbd362b42ccc5c417886cf189b93dd07
scasado-kitmaker <scasado@kitmaker.com>
2016-04-22 15:49:22 +0200

    -Comentarios

--- application/views/alta.php
+++ application/views/alta.php
@@ -16,6 +16,8 @@
 		<p>Texto legal:</p>
 		<p><?=form_textarea('content','Texto legal to complicado shur','readonly')?></p>	
 		<input type="checkbox" name="vehicle" value="Bike"> He leido y acepto la condiciones<br>
+
+		<!-- if que compruebe el saldo de la bbdd del usuario, si es mayor a 0 boton desactivado y aviso de saldo insuficiente, boton de añadir saldo activado-->
 		<?=form_submit('submit', 'Darse de alta')?>
 	</div>
 </body>
