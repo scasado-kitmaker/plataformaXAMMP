90d8bc5aacb072114e13334e21e8e1e48bcf4a09
scasado-kitmaker <scasado@kitmaker.com>
2016-04-26 16:19:26 +0200

    -Creado controlador para webservices,falta "rellenarlo".

---
+++ application/controllers/webservices.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class plataforma extends CI_Controller {
+ //Constructor del controlador
+	public function __construct()
+	{
+		
+	}
+	public function index()
+	{	
+		
+	}
+	public function login()
+	{
+		$host="http://52.30.94.95/";
+		$user="scasado";
+		$pass="BRskzyTE";
+	}
+
+
+
+}
